c353d831cb36e6477fd572de35aee879cadbf569
user <harkovam50@gmail.com>
2025-02-21 10:31:42 +0300

    Last commit working version.

--- src/main/java/com/example/cardealershipwebsite/CarDealershipWebsiteApplication.java
+++ src/main/java/com/example/cardealershipwebsite/CarDealershipWebsiteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /** nothing. */
 @SpringBootApplication
 public class CarDealershipWebsiteApplication {
+
     /** nothing. */
     public static void main(String[] args) {
         SpringApplication.run(CarDealershipWebsiteApplication.class, args);

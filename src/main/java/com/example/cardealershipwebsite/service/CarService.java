a5608d427aefdf8627698132ccf8099ccaedbc70
user <harkovam50@gmail.com>
2025-02-26 22:07:30 +0300

    last commit (work version) services/model

--- src/main/java/com/example/cardealershipwebsite/service/CarService.java
+++ src/main/java/com/example/cardealershipwebsite/service/CarService.java
@@ -9,11 +9,11 @@ public class CarService {
 
     /** Path params. */
     public Car getCarById(Long id) {
-        return new Car(id, "Mercedes", "Black", 20000);
+        return new Car(id, "Mercedes", "Black", 120000);
     }
 
     /** Query params. */
     public Car getCarByNameAndColor(String name, String color) {
-        return new Car(1L, name, color, 19999);
+        return new Car(1L, name, color, 79999);
     }
 }

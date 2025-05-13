b2a2f55ea4ea1e378626833bb5bb95eeccd4725c
Mshk <harkovam50@gmail.com>
2025-05-13 16:03:32 +0300

    Update axiosConfig.ts

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 // Настроим базовый URL для всех запросов
 const axiosInstance = axios.create({
     baseURL: 'https://cardealershipwebsite.onrender.com', // Замените на ваш URL сервера
-    timeout: 100   , // Таймаут для запросов
 });
 
 export default axiosInstance;
